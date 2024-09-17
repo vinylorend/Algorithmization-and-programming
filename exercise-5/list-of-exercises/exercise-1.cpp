d0af2f55ec7a0595dae7c4f06938fac9574005b2
vinylorend <vinylorend@gmail.com>
2024-09-17 19:22:36 +0300

    Exercises

--- exercise-5/list-of-exercises/exercise-1.cpp
+++ exercise-5/list-of-exercises/exercise-1.cpp
@@ -11,26 +11,26 @@ int main() {
     cin >> k;
 
     if (k <= 1) {
-        cout << "ERROR: k <= 1" << endl;
+        cout << "ERROR: k > 1" << endl;
         return 1;
     }
     
-    cout << "Enter the omega frequency value (ω): \t";
+    cout << "Enter the omega frequency value (omega): \t";
     cin >> omega;
 
     if (omega <= 0) {
-        cout << "ERROR: omega <= 0" << endl;
+        cout << "ERROR: omega > 0" << endl;
         return 1;
-
+    }
     phi_0 = asin(1.0 / k);
 
     T = (M_PI / 2 - phi_0) / omega;
 
     x_T = A * sin(omega * T + phi_0);
 
-    cout << "The magnitude of the Phi(0) (phase shift): \t" << phi_0 << endl;
-    cout << "The time point T at which the deviation achieves its maximum significance: \t" << T << endl;
-    cout << "The value of the x(T) coordinate: \t" << x_T << endl;
+    cout << "The magnitude of the Phi(0) (phase shift): " << phi_0 << endl << endl;
+    cout << "The time point T at which the deviation achieves its maximum significance: \t" << T << endl << endl;
+    cout << "The value of the x(T) coordinate: \t" << x_T << endl << endl;
 
     return 0;
 }
