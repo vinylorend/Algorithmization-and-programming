a809929436c4e892afa06c2eebe632fd5e6f8a0a
vinylorend <vinylorend@gmail.com>
2024-09-17 19:22:58 +0300

    Exercises

--- exercise-5/list-of-exercises/exercise-2.cpp
+++ exercise-5/list-of-exercises/exercise-2.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <cmath>
-#include <limits> //for infinity
 
 using namespace std;
 
@@ -16,36 +15,46 @@ int main() {
     cout << "Enter the angle 'alpha' (in degrees): \t";
     cin >> alpha;
 
-    
-    alpha = alpha * M_PI / 180.0; // Converting the angle to radians
+    // Convert angle to radians
+    alpha = alpha * M_PI / 180.0; 
 
+    // Checking value constraints #1
     if (sin(alpha) == 0) {
-        cout << "ERROR: angle < 0 or angle > 180" << endl;
+        cout << "ERROR: sin(alpha) == 0" << endl;
         return 1;
     }
-
+    // Checking value constraints #2
     if (V == 0) {
-        cout << "ERROR: V == 0 << endl" << endl;
+        cout << "ERROR: V == 0" << endl;
         return 1;
     }
 
-    double under_sqrt = 1 + (2 * g * H) / (V * V * sin(alpha) * sin(alpha));
+    double under_sqrt = 1 + (2 * g * H) / (pow(V, 2) * pow(sin(alpha), 2));
 
+    // Checking value constraints #3
     if (under_sqrt < 0) {
         cout << "ERROR: value under sqrt < 0" << endl;
+        return 1;
+    }
+    
+    double under_line = pow(V, 2) * pow(sin(alpha), 2);
+
+    // Checking value constraints #4
+    if (under_line == 0) {
+        cout << "ERROR: value under line == 0" << endl;
         return 1;
     }
 
     T = (V * sin(alpha) / g) * (1 + sqrt(under_sqrt));
 
     x_T = V * T * cos(alpha);
-    y_T = H + V * T * sin(alpha) - 0.5 * g * T * T;
+    y_T = H + V * T * sin(alpha) - 0.5 * g * pow(T, 2);
 
     if (y_T < 0) {
         y_T = 0;
     }
 
-    cout << "The flight time of the stone 'T': " << T << " sec" << endl;
+    // cout << "The flight time of the stone 'T': " << T << " sec" << endl;
     cout << "Horizontal position of the stone 'x(T)': " << x_T << " meters" << endl;
     cout << "The vertical position of the stone 'y(T)': " << y_T << " meters" << endl;
 
